fb69ceedc95411ad1f4134e21dbaadf6591a25aa
A-Shanan <abboudshanan66@gmail.com>
2023-07-23 16:25:33 +0300

    done with AppRouter and create character item WDGT

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,16 +1,35 @@
+import 'package:bloc_training/business_logic/cubit/characters_cubit.dart';
+import 'package:bloc_training/data/repository/characters_repository.dart';
+import 'package:bloc_training/data/web_services/characters_web_services.dart';
 import 'package:bloc_training/presentation/screens/characters_details.dart';
 import 'package:bloc_training/presentation/screens/characters_screen.dart';
 import 'package:flutter/material.dart';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'constants/strings.dart';
 
 class AppRouter {
+  late CharactersRepository charactersRepository;
+  late CharactersCubit charactersCubit;
+
+  AppRouter() {
+    charactersRepository = CharactersRepository(CharactersWebServices());
+    charactersCubit = CharactersCubit(charactersRepository);
+  }
+
   Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case charactersScreen:
-        return MaterialPageRoute(builder: (_) => CharacterScreen());
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (BuildContext context) => charactersCubit,
+            child: CharacterScreen(),
+          ),
+        );
       case characterDetailsScreen:
-        return MaterialPageRoute(builder: (_) => CharacterDetailsScreen());
+        return MaterialPageRoute(
+            builder: (_) => const CharacterDetailsScreen());
     }
   }
 }
--- lib/presentation/screens/characters_screen.dart
+++ lib/presentation/screens/characters_screen.dart
@@ -1,14 +1,88 @@
+import 'package:bloc_training/business_logic/cubit/characters_cubit.dart';
+import 'package:bloc_training/constants/my_colors.dart';
+import 'package:bloc_training/data/models/characters.dart';
+import 'package:bloc_training/presentation/widgets/character_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CharacterScreen extends StatelessWidget {
+class CharacterScreen extends StatefulWidget {
   const CharacterScreen({super.key});
+
+  @override
+  State<CharacterScreen> createState() => _CharacterScreenState();
+}
+
+class _CharacterScreenState extends State<CharacterScreen> {
+  late List<Character> allCharacters;
+
+  @override
+  void initState() {
+    super.initState();
+    allCharacters =
+        BlocProvider.of<CharactersCubit>(context).getAllCharacters();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Characters'),
+        backgroundColor: MyColors.myYellow,
+        title: const Text(
+          'Characters',
+          style: TextStyle(color: MyColors.myGrey),
+        ),
       ),
+    );
+  }
+
+  Widget buildBlocWidget() {
+    return BlocBuilder<CharactersCubit, CharactersState>(
+      builder: (context, state) {
+        if (state is CharactersLoaded) {
+          allCharacters = (state).characters;
+          return buildLoadedListWidgets();
+        } else {
+          return showLoadingIndicator();
+        }
+      },
+    );
+  }
+
+  Widget showLoadingIndicator() {
+    return const Center(
+      child: CircularProgressIndicator(
+        color: MyColors.myYellow,
+      ),
+    );
+  }
+
+  Widget buildLoadedListWidgets() {
+    return SingleChildScrollView(
+      child: Container(
+        color: MyColors.myGrey,
+        child: Column(
+          children: [
+            buildCharactersList(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildCharactersList() {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 2 / 3,
+        crossAxisSpacing: 1,
+        mainAxisSpacing: 1,
+      ),
+      shrinkWrap: true,
+      physics: const ClampingScrollPhysics(),
+      padding: EdgeInsets.zero,
+      itemBuilder: (ctx, index) {
+        return const CharacterItem();
+      },
     );
   }
 }
