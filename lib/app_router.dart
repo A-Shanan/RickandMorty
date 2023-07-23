af988f51b873d80d8186d75bc32e69fe2e887ada
A-Shanan <abboudshanan66@gmail.com>
2023-07-23 13:37:11 +0300

    create constants folder and create details screen

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,11 +1,16 @@
+import 'package:bloc_training/presentation/screens/characters_details.dart';
 import 'package:bloc_training/presentation/screens/characters_screen.dart';
 import 'package:flutter/material.dart';
+
+import 'constants/strings.dart';
 
 class AppRouter {
   Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
-      case '/':
+      case charactersScreen:
         return MaterialPageRoute(builder: (_) => CharacterScreen());
+      case characterDetailsScreen:
+        return MaterialPageRoute(builder: (_) => CharacterDetailsScreen());
     }
   }
 }
---
+++ lib/constants/strings.dart
@@ -0,0 +1,4 @@
+final baseUrl = 'https://rickandmortyapi.com/api/';
+
+const charactersScreen = '/';
+const characterDetailsScreen = '/character_details';
