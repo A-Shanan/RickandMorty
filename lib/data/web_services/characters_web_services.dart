1d911e6092b82e49717720955fc6c27b2e682f40
A-Shanan <abboudshanan66@gmail.com>
2023-07-23 14:35:35 +0300

    create web_services

---
+++ lib/data/web_services/characters_web_services.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: avoid_print
+
+import 'package:bloc_training/constants/strings.dart';
+import 'package:dio/dio.dart';
+
+class CharactersWebServices {
+  late Dio dio;
+
+  CharactersWebServices() {
+    BaseOptions options = BaseOptions(
+      baseUrl: baseUrl,
+      receiveDataWhenStatusError: true,
+      connectTimeout: const Duration(seconds: 25),
+      receiveTimeout: const Duration(seconds: 25),
+    );
+    dio = Dio(options);
+  }
+
+  Future<List<dynamic>> getAllCharacters() async {
+    try {
+      Response response = await dio.get('character');
+      print(response.data.toString());
+      return response.data;
+    } catch (e) {
+      print(e.toString());
+      return [];
+    }
+  }
+}
