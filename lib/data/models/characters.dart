3eb74e127c7773a7b497d8c90a424d2d46a60340
A-Shanan <abboudshanan66@gmail.com>
2023-07-23 14:14:36 +0300

    create characters model

---
+++ lib/data/models/characters.dart
@@ -0,0 +1,29 @@
+class Character {
+  late int id;
+  late String name;
+  late String statusIfDeadOrAlive;
+  late String species;
+  late String type;
+  late String gender;
+  late String originName;
+  late String originUrl;
+  late String locationName;
+  late String locationUrl;
+  late String image;
+  late List<dynamic> episode;
+
+  Character.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json[''];
+    statusIfDeadOrAlive = json['status'];
+    species = json['species'];
+    type = json['type'];
+    gender = json['gender'];
+    originName = json['origin']['name'];
+    originUrl = json['origin']['url'];
+    locationName = json['location']['name'];
+    locationUrl = json['location']['url'];
+    image = json['image'];
+    episode = json['episode'];
+  }
+}
